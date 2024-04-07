5a377b8c1eef56509a90364eed4d79f03c20be26
nconeymmi <allenquicosa@gmail.com>
2024-04-08 06:22:40 +0800

    added get_word_number to main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    wordcount = get_word_number(text)
     print(text)
+    print(f"total number of words: {wordcount}")
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_word_number(text):
+    words = len(text.split())
+    return words
 
 main()
