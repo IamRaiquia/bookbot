96d73c3575535e049e21a52f118655d7eb8c5f0a
nconeymmi <allenquicosa@gmail.com>
2024-04-08 06:55:07 +0800

    added get_letter_count function into main.py

--- main.py
+++ main.py
@@ -2,17 +2,26 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    wordcount = get_word_number(text)
+    wordcount = get_word_count(text)
+    lettercount = get_letter_count(text)
     print(text)
-    print(f"total number of words: {wordcount}")
+    print(f"Count for each word in the document: {wordcount}")
+    print(f"Count for each letter in the document: {lettercount}")
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_word_number(text):
+def get_word_count(text):
     words = len(text.split())
     return words
+
+def get_letter_count(text):
+    lowercase_text = text.lower()
+    count_per_letter = {}
+    for letter in range(ord('a'), ord('z') + 1):
+        count_per_letter[chr(letter)] = lowercase_text.count(chr(letter))
+    return count_per_letter
 
 main()
